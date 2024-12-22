222cd5e887fe6133fda9c020b5ef25e0eb0adc52
IamYubrajKhatri <khatriyubraj1157@gmail.com>
2024-12-22 17:33:33 +0100

    noch

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,20 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+export default defineConfig(({ mode }) => {
+  const isProduction = mode === 'production';
 
-/* Access the environment variable
-const apiUrl = process.env.NODE_ENV === 'production'
-  ? 'https://setflix.azurewebsites.net' // Production URL
-  : 'http://localhost:4000'; // Local backend URL*/
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server:{
-    proxy:{
-      "/api":{
-        target:'https://setflix1.azurewebsites.net',
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        "/api": {
+          target: isProduction
+            ? 'https://setflix1.azurewebsites.net' // Production URL
+            : 'http://localhost:4001', // Local backend URL
+          changeOrigin: true,
+          secure: false, // Disable if your local server uses self-signed certificates
+        }
       }
     }
-  }
-})
+  };
+});
