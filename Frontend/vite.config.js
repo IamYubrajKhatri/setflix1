27d089a115008cb2ae5ff6fcc4058dfec44e7725
IamYubrajKhatri <khatriyubraj1157@gmail.com>
2024-12-22 18:59:28 +0100

    2 nd

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,17 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-  const isProduction = mode === 'production';
+export default defineConfig(() => {
+  //const isProduction = mode === 'production' ; { mode}
 
   return {
     plugins: [react()],
     server: {
       proxy: {
         "/api": {
-          target: isProduction
-            ? 'https://setflix1.azurewebsites.net' // Production URL
-            : 'http://localhost:4001', // Local backend URL
+          target:// Production URL
+             'http://localhost:4001', // Local backend URL
           changeOrigin: true,
           secure: false, // Disable if your local server uses self-signed certificates
         }
